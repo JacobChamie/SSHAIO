8c413678b22b37783570e6e752b1d8714bded1a4
JackH6454 <jack64hohn@gmail.com>
2015-04-05 18:36:28 -0700

    Create SSHAIO.h

---
+++ V0.0.3/SSHAIO.h
@@ -0,0 +1,28 @@
+//VERSION = 0.0.3
+//Author: Jack Hohn
+
+#ifndef SSHAIO_h
+#define SSHAIO_h
+
+#include "Arduino.h"
+
+class SSHAIO
+{
+	public:
+	static void hello();
+	static void unknown();
+	static void inputSystem();
+	static void memoryFree();
+	static void loadText();
+	static void PIN12HIGH();
+	static void PIN12LOW();
+	static void PIN8HIGH();
+	static void PIN8LOW();
+	static void PIN7HIGH();
+	static void PIN7LOW();
+	static void PIN2HIGH();
+	static void PIN2LOW();
+	static void SDBegin();
+	static void cat();
+};
+#endif
